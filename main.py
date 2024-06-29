d75423cbfb9e50f56b2edd25f3e0bbcd427272b1
Rafey <rafeldo36@gmail.com>
2024-06-29 14:25:33 +0530

    Python Re

---
+++ main.py
@@ -0,0 +1,107 @@
+# #first print statement in python
+# print("Bismillah")
+
+# #comments in python
+# #this is a single line comment
+# ''' this is multiline
+# string comment'''
+
+# #escape sequence characters
+# print("Hello \n World") #new line
+
+# #separator and end parameters in print statement
+# print("Rafey", sep='@', end="Ansari")
+
+# #types of datatypes in python
+# a = 1
+# b = True
+# c = "Harry"
+# d = None
+# e = 9.9
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# #sequence datatypes
+# #list
+# list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+# print(list1)
+
+# #tuple
+# tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+# print(tuple1)
+
+# #dictionary
+# dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+# print(dict1)
+
+# #calculator
+
+# n = 15
+# m = 7
+# ans1 = n+m
+# print("Addition of",n,"and",m,"is", ans1)
+# ans2 = n-m
+# print("Subtraction of",n,"and",m,"is", ans2)
+# ans3 = n*m
+# print("Multiplication of",n,"and",m,"is", ans3)
+# ans4 = n/m
+# print("Division of",n,"and",m,"is", ans4)
+# ans5 = n%m
+# print("Modulus of",n,"and",m,"is", ans5)
+# ans6 = n//m
+# print("Floor Division of",n,"and",m,"is", ans6)
+
+# #typecasting in python
+# #explicit
+# string = "15"
+# number = 7
+# string_number = int(string) #throws an error if the string is not a valid integer
+# sum= number + string_number
+# print("The Sum of both the numbers is: ", sum)
+
+# #implicit
+# # Python automatically converts
+# # a to int
+# a = 7
+# print(type(a))
+ 
+# # Python automatically converts b to float
+# b = 3.0
+# print(type(b))
+ 
+# # Python automatically converts c to float as it is a float addition
+# c = a + b
+# print(c)
+# print(type(c))
+
+# #taking user input
+# name=input("Enter your name: ")
+# print(name)
+
+# age=int(input("Enter your age: "))
+# print(age)
+
+# #string methods
+# name = "Rafey"
+# print("Hey, " + name)
+
+# print('He said, "I want to eat an apple".')
+
+# #multiline strings
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+# #Accessing character in string
+# print(name[0])
+# print(name[1])
+
+# #loopin in string
+# for character in name:
+#     print(character)
+
