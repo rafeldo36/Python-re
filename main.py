b7d7df3e4326488033b2f99f04b8890cf813a114
Rafey <rafeldo36@gmail.com>
2024-06-30 16:29:06 +0530

    recursion

--- main.py
+++ main.py
@@ -593,3 +593,14 @@
 #     return n**2
 # print(square.__doc__)
 
+# Recursion 
+def factorial(num): 
+    if (num == 1 or num == 0):
+        return 1
+    else:
+        return (num * factorial(num - 1)) 
+  
+# Driver Code 
+num = 7; 
+print("Number: ",num)
+print("Factorial: ",factorial(num))
