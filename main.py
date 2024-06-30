f288233acdbd0888059fc9d23c9f43f11545f514
Rafey <rafeldo36@gmail.com>
2024-06-30 16:57:39 +0530

    finally clause

--- main.py
+++ main.py
@@ -791,3 +791,13 @@
 #     num = int(input("Enter an integer: "))
 # except ValueError:
 #     print("Number entered is not an integer.")
+
+# finally clause
+try:
+    num = int(input("Enter an integer: "))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("Integer Accepted.")
+finally:
+    print("This block is always executed.")
