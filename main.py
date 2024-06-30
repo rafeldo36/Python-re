a6b58694242f85d4643c940ae2a7975db5bd9131
Rafey <rafeldo36@gmail.com>
2024-06-30 16:39:03 +0530

    joining sets

--- main.py
+++ main.py
@@ -616,3 +616,49 @@
 # info = {"Carla", 19, False, 5.9}
 # for item in info:
 #     print(item)
+
+# # Joining Sets
+
+# # union() and update()
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities3 = cities.union(cities2)
+# print(cities3)
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities.update(cities2)
+# print(cities)
+
+# # intersection and intersection_update()
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities3 = cities.intersection(cities2)
+# print(cities3)
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities.intersection_update(cities2)
+# print(cities)
+
+# # symmetric_difference and symmetric_difference_update()
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities3 = cities.symmetric_difference(cities2)
+# print(cities3)
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities.symmetric_difference_update(cities2)
+# print(cities)
+
+# # difference() and difference_update()
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Seoul", "Kabul", "Delhi"}
+# cities3 = cities.difference(cities2)
+# print(cities3)
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Seoul", "Kabul", "Delhi"}
+# print(cities.difference(cities2))
+
