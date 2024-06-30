d1bb5c484f3a03b1be2c0f98b16f0d666e61b509
Rafey <rafeldo36@gmail.com>
2024-06-30 11:56:58 +0530

    functions arguments

--- main.py
+++ main.py
@@ -369,3 +369,46 @@
 #         continue
 #     print(i)
 
+# #functions
+# def name(fname, lname):
+#     print("Hey,", fname, lname)
+# name("Rafey", "Ansari")
+
+# #Function Arguments and return statement
+
+# #default arguments
+# def name(fname, mname = "Jhon", lname = "Whatson"):
+#     print("Hello,", fname, mname, lname)
+
+# name("Amy")
+
+# # Keyword arguments
+# def name(fname, mname, lname):
+#     print("Hello,", fname, mname, lname)
+
+# name(mname = "Peter", lname = "Wesker", fname = "Jade")
+
+# # Required arguments:
+# def name(fname, mname, lname):
+#     print("Hello,", fname, mname, lname)
+
+# name("Peter", "Ego", "Quill")
+
+# # variable length arguments
+# #Arbitrary Arguments
+# def name(*name):
+#     print("Hello,", name[0], name[1], name[2])
+
+# name("James", "Buchanan", "Barnes")
+
+# # Keyword Arbitrary Arguments:
+# def name(**name):
+#     print("Hello,", name["fname"], name["mname"], name["lname"])
+
+# name(mname = "Buchanan", lname = "Barnes", fname = "James")
+
+# return Statement
+def name(fname, mname, lname):
+    return "Hello, " + fname + " " + mname + " " + lname
+
+print(name("James", "Buchanan", "Barnes"))
