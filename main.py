25026d8489b4f001aeacaa5d520f6424339b4455
Rafey <rafeldo36@gmail.com>
2024-06-30 16:54:38 +0530

    else in loop

--- main.py
+++ main.py
@@ -778,3 +778,10 @@
 # info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
 # del info
 # print(info)
+
+# else in loop
+for x in range(5):
+    print ("iteration no {} in for loop".format(x+1))
+else:
+    print ("else block in loop")
+print ("Out of loop")
