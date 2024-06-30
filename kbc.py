bed564a4e82ef0e81774e2155cc0dd9177a9b9eb
Rafey <rafeldo36@gmail.com>
2024-06-30 16:26:08 +0530

    f strings and docstrings

---
+++ kbc.py
@@ -0,0 +1,25 @@
+# Creating a game like kbc using list
+
+data=[
+   ["Which planet in the solar system is known as the “Red Planet”?",
+    "Mars", "Jupiter", "Saturn", "Uranus",1,5000
+    ],
+   [
+       "What is the capital of Japan?",
+       "Beijing",'Tokyo',"Seoul" ,"Bangkok", 2,10000
+   ],
+   [
+       " Which river is the longest in the world?",
+       "Amazon",'Ganga',"Nile" ,"Yangtze", 3,20000
+   ],
+   [
+       " What animal is the national symbol of Australia?",
+       "Kangaroo",'Koala',"Emu" ,"Crocodile", 1,30000
+   ],
+   [
+       "What chemical element is designated as “Hg”?",
+       "Silver",'Tin',"Copper" ,"Mercury", 2,50000
+   ],
+]
+
+
--- main.py
+++ main.py
@@ -568,3 +568,28 @@
 # Tuple = (0, 1, 2, 3, 2, 3, 1, 3, 2)
 # res = Tuple.index(3)
 # print('First occurrence of 3 is', res)
+
+# # F string
+# txt = "For only {price:.2f} dollars!"
+# print(txt.format(price = 49))
+
+# val = 'Geeks'  
+# print(f"{val}for{val} is a portal for {val}.")  
+# name = 'Tushar'  
+# age = 23  
+# print(f"Hello, My name is {name} and I'm {age} years old.")
+
+# print(f"{2 * 30}")
+
+# # docstrings
+
+# def square(n):
+#     '''Takes in a number n, returns the square of n'''
+#     return n**2
+# print(square(5))
+
+# def square(n):
+#     '''Takes in a number n, returns the square of n'''
+#     return n**2
+# print(square.__doc__)
+
