3595d8c8eec557689c6b408bef6e0126334ab166
Rafey <rafeldo36@gmail.com>
2024-07-22 16:15:08 +0530

    seek, tell, truncate

--- main.py
+++ main.py
@@ -934,4 +934,31 @@
 #     f.write(line + '\n')
 # f.close()
 
-    
+
+# # seek
+# with open('myfile.txt', 'r') as f:
+#   # Move to the 10th byte in the file
+#   f.seek(10)
+
+#   # Read the next 5 bytes
+#   data = f.read(5)
+#   print(data)
+
+# # tell()
+# with open('myfile.txt', 'r') as f:
+#   # Read the first 10 bytes
+#   data = f.read(10)
+
+#   # Save the current position
+#   current_position = f.tell()
+
+#   # Seek to the saved position
+#   f.seek(current_position)
+
+# # truncate()
+# with open('sample.txt', 'w') as f:
+#   f.write('Hello World!')
+#   f.truncate(5)
+
+# with open('sample.txt', 'r') as f:
+#   print(f.read())
