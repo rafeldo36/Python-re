36c8864136f3ee9424063ad69b3c6058209b538d
Rafey <rafeldo36@gmail.com>
2024-07-22 17:23:09 +0530

    Getters and Setters

--- main.py
+++ main.py
@@ -1107,3 +1107,32 @@
 # hello()
 # # greet(add)(1, 2)
 # add(1, 2)
+
+# # Getters and Setters
+
+# # getters
+# class MyClass:
+#     def __init__(self, value):
+#         self._value = value
+
+#     @property
+#     def value(self):
+#         return self._value
+# obj = MyClass(10)
+# print(obj.value)
+
+# # setters
+# class MyClass:
+#     def __init__(self, value):
+#         self._value = value
+
+#     @property
+#     def value(self):
+#         return self._value
+
+#     @value.setter
+#     def value(self, new_value):
+#         self._value = new_value
+# obj = MyClass(10)
+# obj.value = 20
+# print(obj.value)
