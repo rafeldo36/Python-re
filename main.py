b12848c46c413fe46b9e72647dfb4ddfcc05bcca
Rafey <rafeldo36@gmail.com>
2024-07-22 16:31:41 +0530

    'is' vs '==' in Python

--- main.py
+++ main.py
@@ -1013,3 +1013,24 @@
 
 # # Print the sum
 # print(sum)
+
+# # 'is' vs '==' in Python
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+
+# print(a == b)  # True
+# print(a is b)  # False
+
+# a = "hello"
+# b = "hello"
+
+# print(a == b)  # True
+# print(a is b)  # True
+
+# a = 5
+# b = 5
+
+# print(a == b)  # True
+# print(a is b)  # True
+
