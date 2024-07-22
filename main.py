61eb9239587dabc940f8602bb94ffcde7d47ec9f
Rafey <rafeldo36@gmail.com>
2024-07-22 17:18:18 +0530

    Python Decorators

--- main.py
+++ main.py
@@ -1072,3 +1072,38 @@
 #   def __init__(self):
 #     print("Crab belongs to Crustaceans group")
 # obj1=Details()
+
+# # Python Decorators
+# import logging
+
+# def log_function_call(func):
+#     def decorated(*args, **kwargs):
+#         logging.info(f"Calling {func.__name__} with args={args}, kwargs={kwargs}")
+#         result = func(*args, **kwargs)
+#         logging.info(f"{func.__name__} returned {result}")
+#         return result
+#     return decorated
+
+# @log_function_call
+# def my_function(a, b):
+#     return a + b
+
+# def greet(fx):
+#   def mfx(*args, **kwargs):
+#     print("Good Morning")
+#     fx(*args, **kwargs)
+#     print("Thanks for using this function")
+#   return mfx
+
+# @greet
+# def hello():
+#   print("Hello world")
+
+# @greet
+# def add(a, b):
+#   print(a+b)
+  
+# # greet(hello)()
+# hello()
+# # greet(add)(1, 2)
+# add(1, 2)
